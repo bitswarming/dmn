efb045bce7d7903162dce14257fbcf2646f23cf3
bit swarm <bitswarming@gmail.com>
2018-06-06 11:13:08 +0300

    Update i.sh

--- i.sh
+++ i.sh
@@ -14,23 +14,24 @@ curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.li
 sudo apt-get update
 sudo apt-get install -y nvidia-docker2
 sudo pkill -SIGHUP dockerd
-echo "3. install docker"
-sudo apt-get install \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
-apt update
-apt-get install docker-ce
+#echo "3. install docker"
+#sudo apt-get install \
+#    apt-transport-https \
+#    ca-certificates \
+#    curl \
+#    software-properties-common
+#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+#sudo add-apt-repository \
+#   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+#   $(lsb_release -cs) \
+#   stable"
+#apt update
+#apt-get install docker-ce
 echo "4. install docker compose"
 curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 echo "5. starting verai"
+docker pull  verai/jnbk1
 docker network create jupyterhub-network
 docker volume create --name=jupyterhub-data
 docker volume create --name=jupyterhub-db-data
